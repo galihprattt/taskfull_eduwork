cf0373a6fb0c8535e6fd6686ec3978330b63b959
Galih Pratama prat <galihprat43@gmail.com>
2025-02-14 12:55:26 +0700

    fe_sisco

---
+++ sisco-aja/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="header">
+  <div class="container navbar">
+    <!-- Logo -->
+    <div class="logo">
+      <b>Emporio</b></div>
+
+    <!-- Search Bar -->
+    <div class="search-bar">
+      <input type="text" placeholder="Search" />
+      
+    </div>
+
+    <!-- Navigation Links -->
+    <nav class="nav-links">
+      <b><a href="#">New Products</a>
+      <a href="#">Men</a>
+      <a href="#">Women</a>
+      <a href="#">Login</a>
+      <a href="#" class="signup-button">Sign Up</a>
+    </b>
+    </nav>
+  </div>
+  
+  <div class="bg-image">
+    <div class="container">
+      <img src="@/assets/gambaran.jpg" alt="Gambaran" />
+    </div>
+    </div>
+  
+
+  
+</header>
+
+  <RouterView />
+</template>
---
+++ sisco-aja/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+// Produk yang ditampilkan di bagian utama
+let products = ref([]);
+
+// Fungsi mendapatkan daftar produk
+function getProduct() {
+  axios
+    .get("https://sistemtoko.com/public/demo/product")
+    .then((res) => {
+      products.value = res.data.aaData;
+    })
+    .catch((err) => alert(err));
+}
+
+getProduct();
+</script>
+
+<template>
+  
+
+  <main>
+    <div class="container">
+      <center><h2>Featured Product</h2></center>
+      <div class="row">
+        <div class="col-md-4 mb-4" v-for="(product, idx) in products" :key="idx">
+          <div class="card">
+            <div class="card-body">
+              <img :src="product.photo" alt="" width="100%" />
+              <span>{{ product.name }}</span>
+              <br />
+              <b><span>Rp. {{ product.price }}</span></b>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
